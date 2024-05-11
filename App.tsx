ab1a81efc15a66b0e9b012771accdf7713bcb690
Khalil Augusto Salomâo Amaral <100626755+Chat809@users.noreply.github.com>
2024-05-10 21:39:48 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -4,19 +4,18 @@ import ServiceCard from './components/ServiceCard';
 import Header from './components/Header';
 import HomeContent from './components/HomeContent';
 import Footer from './components/Footer';
-import Slider from './components/Slider';
 import TeamCard from './components/TeamCard';
 // @ts-ignore
 import landingPageImg from './IMGS/img-landingpage.jpg';
 // @ts-ignore
 import khalilImg from './IMGS/img_khalil.jpeg';
+import FAQCard from './components/FAQCard';
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
       <HomeContent />
-      <Slider />
       <div className="box-content-services-portfolio">
         <div className="title-content poppins">Nossos Serviços</div>
         <div className="box-cards-services">
@@ -29,9 +28,21 @@ const App: React.FC = () => {
         </div>
       </div>
       <div className='box-content-team'>
+      <div className='title-content alegreya'>Desenvolvedores</div>
+      <div className='box-cards-team'>
         <TeamCard avatarName="Khalil Salomão" avatarDescription="Fullstack Developer" avatarImg={khalilImg} />
         <TeamCard avatarName="Khalil Salomão" avatarDescription="Fullstack Developer" avatarImg={khalilImg} />
         <TeamCard avatarName="Khalil Salomão" avatarDescription="Fullstack Developer" avatarImg={khalilImg} />
+      </div>
+      </div>
+      <div className='box-content-FAQs'>
+        <div className='title-content alegreya'>Dúvidas?</div>
+        <div className='box-cards-FAQs'>
+          <FAQCard name='Qual garantia a ravatech oferece?' description='Nós oferecemos via multa prevista no contrato, além da entrega gratuita do que for desenvolvido até então' />
+          <FAQCard name='Como funciona o aluguel de sites?' description='Nosso serviço de aluguel de sites permite que você utilize um site profissional sem a necessidade de ivestir em um próprio. Você paga uma taxa mensal e nós adaptamos de acordo com sua necessidade' />
+          <FAQCard name='Qual o prazo estimado da entrega?' description='O prazo estimado de entrega varia de acordo com o serviço contratado e a complexidade do projeto. Para landing pages, de modo geral entregamos em até uma semana.' />
+          <FAQCard name='Quais as formas de pagamento?' description='Aceitamos várias formas de pagamento, incluindo cartão de crédito, transferência bancária e PayPal. Entre em contato conosco para mais informações sobre formas de pagamento disponíveis.' />
+        </div>
       </div>
       <Footer />
     </div>
